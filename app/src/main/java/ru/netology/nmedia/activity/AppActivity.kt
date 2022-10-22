43d7d5d288bbf11329197fe6062dae008337d1a7
SumbulIshdavletova <ishdavletova.sumbul@gmail.com>
2022-10-22 17:07:00 +0500

    Notification

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -2,8 +2,11 @@ package ru.netology.nmedia.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.R
 import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
@@ -33,6 +36,21 @@ class AppActivity : AppCompatActivity() {
                     textArg = text
                 })
         }
+        checkGoogleApiAvailability()
 
+    }
+
+    private fun checkGoogleApiAvailability() {
+        with(GoogleApiAvailability.getInstance()) {
+            val code = isGooglePlayServicesAvailable(this@AppActivity)
+            if (code == ConnectionResult.SUCCESS) {
+                return@with
+            }
+            if (isUserResolvableError(code)) {
+                getErrorDialog(this@AppActivity, code, 9000)?.show()
+                return
+            }
+            Toast.makeText(this@AppActivity, "Google Api Unavailable", Toast.LENGTH_LONG).show()
+        }
     }
 }
